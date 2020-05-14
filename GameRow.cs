b8ac2f56038eda539a8d080113eb8f32515dded5
developer313 <ah.parsapour@gmail.com>
2020-05-14 11:11:19 +0430

    First Commit

---
+++ GameRow.cs
@@ -0,0 +1,7 @@
+﻿namespace MasterMind
+{
+    public class GameRow
+    {
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MasterMind
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
